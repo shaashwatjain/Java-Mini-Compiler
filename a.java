7e1b0b27d9ca8129318e4348c87b84991ab25ceb
Shaashwat <shaashjain213@gmail.com>
2021-05-03 18:05:27 +0530

    All changes to the compiler

---
+++ a.java
@@ -0,0 +1,33 @@
+class Test
+{
+	//Comment 1
+	int b;
+	public static int main()
+	{
+		int a = 6, c = 7, b = 3;
+		a = 5;
+		c -= 6 / 7;
+		d = 7;
+		for(int i = 0 ; i != 11 ; i++)
+		{
+			a = a + 1;
+		}
+		//b = a;
+		/*int a=++5,b;
+		char d;
+		c = a+b*5-(5/s);*/
+		a = 5;
+
+		switch(c+5)
+		{
+			case 5: a = 0;
+					break;
+			case 6: a = 1;
+					break;
+			default: a = 2;
+		}
+		c = 5;
+		c = a[6][5];
+	}
+	int c;
+}
---
+++ symbol_table/test.java
@@ -0,0 +1,29 @@
+class Test
+{
+	//Comment 1
+	public static int main()
+	{
+		int a = 0;
+		int b = 1;
+		int c = 2;
+		for(a = 2; a < 10; a = a+1)
+		{
+
+			b += 1;
+		}
+		//b = a;
+		/*int a=++5,b;
+		char d;
+		c = a+b*5-(5/s);*/
+		a = 5;
+
+		switch(a)
+		{
+			case (5): a = 5;
+					break;
+			case (6): a = 1;
+					eak;
+
+		}
+	}
+}
